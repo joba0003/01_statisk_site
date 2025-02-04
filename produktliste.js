79641c59c4be8d566d49eeaf79be1a16f3a475b7
Josefine Banke <joba0003@student.kea.dk>
2025-02-04 13:40:41 +0100

    add produktliste js

--- produktliste.js
+++ produktliste.js
@@ -4,12 +4,40 @@ const mycategory = new URLSearchParams(window.location.search).get("category");
 fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
+
 function showList(products) {
   console.log(products);
-  let markup = "";
+
+  const markup = products
+    .map(
+      (product) => `
+      <div class="apparel ${product.soldout ? "apparel_soldOut" : ""}">
+        ${product.soldout ? '<div class="soldout">Sold Out</div>' : ""}
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
+        <p class="overskrift">${product.productdisplayname}</p>
+        <p>${product.articletype} | ${product.brandname}</p>
+        <p>DKK ${product.price},-</p>
+        ${product.discount ? `<div class="discounted"><p>NOW DKK ${product.discount},-</p><p class="percent">-${product.discountpercent}%</p></div>` : ""}
+        <a href="produkt.html?id=${product.id}">Read More</a>
+      </div>`
+    )
+    .join("");
+
+  console.log(markup);
+  listContainer.innerHTML = markup;
+}
+/* const listContainer = document.querySelector(".produktliste");
+const mycategory = new URLSearchParams(window.location.search).get("category");
+
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+function showList(products) {
+  console.log(products);
+  const markup = products;
   products
-    .map((product) => {
-      markup += `<div class="apparel">
+    .map((product) => 
+     `<div class="apparel">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp" alt="bluetee" />
           <p class="overskrift">Sahara Team India Fanwear Round Neck Jersey</p>
           <p>Tshirts | Nike</p>
@@ -73,11 +101,8 @@ function showList(products) {
           <p>DKK 1595,-</p>
           <a href="produkt.html">Read More</a>
         </div>`;
-    })
     .join("");
   console.log(markup);
-  listContainer.innerHTML = markup;
-}
-
-/* const markup = product 
-.map {(product) =>} */
+  listContainer.innerHTML = markup;)
+    
+} */
